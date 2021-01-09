3a590c0b27710bb4bdff02b59c5c0f3a6ca37180
Chawki Ifaoui <chawkiifaoui@gmail.com>
2021-01-09 15:13:44 +0100

    added: server

---
+++ json-server/public/precache-manifest.79f4b2be35eb1f8c3331b9774cd62f6b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61f97aa71a65b1b9021f2d031af305c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77ba0c13b8ed39a48601",
+    "url": "/static/css/2.73dee464.chunk.css"
+  },
+  {
+    "revision": "53196f13eaa5f36d0284",
+    "url": "/static/css/main.0abdb5a7.chunk.css"
+  },
+  {
+    "revision": "77ba0c13b8ed39a48601",
+    "url": "/static/js/2.936f94fe.chunk.js"
+  },
+  {
+    "revision": "1d9ed9265a32ea4776472f445cf2df78",
+    "url": "/static/js/2.936f94fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53196f13eaa5f36d0284",
+    "url": "/static/js/main.43883f85.chunk.js"
+  },
+  {
+    "revision": "aff20233fc33123ab008",
+    "url": "/static/js/runtime-main.74f98a6f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
